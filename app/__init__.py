19f8ad968570646910323febbdc5c0587deba45d
Tahmim Hassan <thassan50@stuy.edu>
2025-05-08 10:35:47 -0400

    added preliminary flask app

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+import json
+import randomCountryimport sqlite3
+from flask import Flask, render_template, request, session, redirect, flash, url_for
+
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True
+    app.run()
